0f68f8c818aab0f944649e1a4fa75ef8d783b5a6
shinyumi <yumishin43@gmail.com>
2022-07-10 23:19:48 +0900

    Array right shift

---
+++ W1_P2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Array {
+private:
+	int* arr;
+	int arrSize;
+public:
+	Array(int size) {
+		this->arrSize = size;
+		this->arr = new int[arrSize];
+		for (int i = 0; i < arrSize; i++) {
+			arr[i] = 0;
+		}
+	}
+
+	void Insert() {
+		for (int i = 0; i < arrSize; i++) {
+			cin >> arr[i];
+		}
+	}
+
+	void Print() {
+		for (int i = 0; i < arrSize; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void Shift() {
+		int sf = arr[arrSize - 1];
+		for (int i = arrSize - 2; i >= 0; i--) {
+			arr[i + 1] = arr[i];
+		}
+		arr[0] = sf;
+	}
+};
+
+int main() {
+	int T, N, D;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> N >> D;
+		Array ar(N);
+		ar.Insert();
+		for (int j = 0; j < D; j++) {
+			ar.Shift();
+		}
+		ar.Print();
+	}
+	return 0;
+}
