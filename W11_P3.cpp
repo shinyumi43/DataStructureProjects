749c4462da0544d4de2630577a6dca550a820e08
shinyumi <yumishin43@gmail.com>
2022-08-22 10:37:50 +0900

    Binary Search Tree insert, inorder

---
+++ W11_P3.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+private:
+	Node* left;
+	Node* right;
+	Node* par;
+	int data;
+
+public:
+	Node(int data) {
+		this->data = data;
+		this->left = nullptr;
+		this->right = nullptr;
+		this->par = nullptr;
+	}
+
+	void setRight(Node* node) {
+		if (node == nullptr) { this->right = nullptr; }
+		else {
+			this->right = node;
+			node->par = this;
+		}
+	}
+
+	void setLeft(Node* node) {
+		if (node == nullptr) { this->left = nullptr; }
+		else {
+			this->left = node;
+			node->par = this;
+		}
+	}
+
+	friend class BST;
+};
+
+class BST {
+public:
+	Node* root;
+
+	BST() { this->root = nullptr; }
+
+	void insert(int data) {
+		Node* node = new Node(data);
+		if (this->root == nullptr) { this->root = node; return; }
+
+		Node* parN = nullptr;
+		Node* curN = this->root;
+
+		while (curN != nullptr) {
+			if (data > curN->data) {
+				parN = curN;
+				curN = curN->right;
+			}
+			else {
+				parN = curN;
+				curN = curN->left;
+			}
+		}
+
+		if (data > parN->data) { parN->setRight(node); }
+		else if (data < parN->data) { parN->setLeft(node); }
+	}
+
+	void inorder(Node* node) {
+		if (node != nullptr) {
+			inorder(node->left);
+			cout << node->data << ' ';
+			inorder(node->right);
+		}
+	}
+};
+
+int main() {
+	int T, N, n;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> N;
+		BST BS;
+		for (int j = 0; j < N; j++) {
+			cin >> n;
+			BS.insert(n);
+		}
+		BS.inorder(BS.root);
+		cout << endl;
+	}
+	return 0;
+}
